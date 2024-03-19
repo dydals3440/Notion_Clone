9466ab3457e1d52abb83a2ab1150ae304628e083
김용민 <dydals3440@gmail.com>
2024-03-19 21:39:10 +0900

    feat: trash-box component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -27,6 +27,7 @@ import {
 import UserItem from './UserItem';
 
 import { DocumentList } from './document-list';
+import TrashBox from './trash-box';
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -164,7 +165,7 @@ export const Navigation = () => {
               className='p-0 w-72'
               side={isMobile ? 'bottom' : 'right'}
             >
-              <p>Trash box</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
